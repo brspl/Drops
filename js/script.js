9cf92a82b8ae7fa631b0c78631a482fe45a0ec6d
BrS <BrS@BrS.com>
2025-09-23 15:07:17 +0200

    Code refactoring

--- js/script.js
+++ js/script.js
@@ -1,21 +1,13 @@
     function calculate() {
-      let timer;
       const DROPTIME = Number(document.getElementById("droptime").value) * 3600;
       const TIMELEFT = 100 - Number(document.getElementById("timeleft").value);
       let result = Math.floor(DROPTIME * TIMELEFT / 100);
 
-      let HOURS = Math.floor(result / 3600);
-      let MINUTES = Math.floor((result % 3600) / 60);
-      let SECONDS = result % 60;
-
-      document.getElementById("result").textContent =
-        `Pozostało: ${HOURS}h ${MINUTES}m ${SECONDS}s`;
-
-      timer = setInterval(function() {
+      let timer = setInterval(function() {
         result--;
-        HOURS = Math.floor(result / 3600);
-        MINUTES = Math.floor((result % 3600) / 60);
-        SECONDS = result % 60;
+        let HOURS = Math.floor(result / 3600);
+        let MINUTES = Math.floor((result % 3600) / 60);
+        let SECONDS = result % 60;
         document.title = `Pozostało: ${HOURS}h:${MINUTES}m:${SECONDS}s`;
         document.getElementById("result").textContent =
           `Pozostało: ${HOURS}h ${MINUTES}m ${SECONDS}s`;
